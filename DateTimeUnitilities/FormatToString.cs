9b2fc689a68f6192f59d24d5888e12ee20a19e03
LHTrungSkySP <lehoangtrung038582@gmail.com>
2024-09-16 00:22:51 +0700

    init

---
+++ DateTimeUnitilities/FormatToString.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unitilities.DateTimeUnitilities;
+
+public class FormatToString
+{
+    /// <summary>
+    /// trả về dateTime chuyển thành string để tạo tên folder/file
+    /// </summary>
+    /// <param name="dateTime">thời gian muốn convert</param>
+    /// <param name="format">format muốn lây</param>
+    /// <returns></returns>
+    static public string FormatToPath(DateTime dateTime, string format)
+    {
+        // Chuyển đổi DateTime sang chuỗi theo format "dd/MM/yyyy"
+        string formattedDate = dateTime.ToString(format);
+
+        // Thay thế ký tự "/" thành "_"
+        formattedDate = formattedDate.Replace("/", "_");
+
+        return formattedDate;
+    }
+    static public string FormatToPath(string format)
+    {
+        // Chuyển đổi DateTime sang chuỗi theo format "dd/MM/yyyy"
+        string formattedDate = DateTime.UtcNow.Date.ToString(format);
+
+        // Thay thế ký tự "/" thành "_"
+        formattedDate = formattedDate.Replace("/", "_");
+
+        return formattedDate;
+    }
+}
+
+
+
+
+
